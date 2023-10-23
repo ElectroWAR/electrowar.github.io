f1764e82f4a066f784e24a3978c39747941b6962
ElectroWAR <electrowar@gmail.com>
2023-10-23 02:33:17 +0100

    components & layouts

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="bg-rose-900 p-4 block" >
+    Header
+  </header>
+  <slot/>
+<footer class="bg-emerald-700 p-4">
+  Footer
+</footer>
+
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  TEST
+  <h1 class="mx-auto p-4 text-3xl">Welcome </h1>
 </template>
